523efe1c1368d918ce622109660053aef01b452d
sebastientromp <sebastien.tromp@gmail.com>
2018-12-10 22:31:42 +0100

    technical: limit TagChanges to the tags actually needed to recreate a replay

--- src/js/services/parser/state-processor.service.ts
+++ src/js/services/parser/state-processor.service.ts
@@ -15,6 +15,30 @@ import { ChangeEntityHistoryItem } from '../../models/history/change-entity-hist
 @Injectable()
 export class StateProcessorService {
 
+    private readonly USEFUL_TAGS: ReadonlyArray<GameTag> = [
+        GameTag.TAG_SCRIPT_DATA_NUM_1,
+        GameTag.TAG_SCRIPT_DATA_NUM_2,
+        GameTag.PREMIUM,
+        GameTag.STEP,
+        GameTag.TURN,
+        GameTag.HERO_ENTITY,
+        GameTag.PLAYER_ID,
+        GameTag.DAMAGE,
+        GameTag.HEALTH,
+        GameTag.ATK,
+        GameTag.COST,
+        GameTag.ZONE,
+        GameTag.CONTROLLER,
+        GameTag.DURABILITY,
+        GameTag.CARDTYPE,
+        GameTag.SECRET,
+        GameTag.ZONE_POSITION,
+        GameTag.MULLIGAN_STATE,
+        GameTag.CREATOR,
+        GameTag.PARENT_CARD,
+        GameTag.QUEST,
+    ];
+    
 	public populateIntermediateStates(game: Game, history: ReadonlyArray<HistoryItem>): Game {
         let previousStateEntities: Map<number, Entity> = game.entities;
         let currentActionIndexInTurn = 0;
@@ -73,6 +97,12 @@ export class StateProcessorService {
     }
 
     private updateWithTagChange(historyItem: TagChangeHistoryItem, entities: Map<number, Entity>): Map<number, Entity> {
+        // Only a limited number of tags are useful for replay reconstitution. If the tag isn't 
+        // one of them, we simply ignore it. Thanks to this, we will have less differences 
+        // between our entities, which will improve the memory footprint and performances
+        if (this.USEFUL_TAGS.indexOf(historyItem.tag.tag) === -1) {
+            return entities;
+        }
         // No default creation - if the entity is not registered yet, it's a bug
         const entity: Entity = entities
                 .get(historyItem.tag.entity)
