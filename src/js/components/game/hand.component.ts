25c0a7adcb19b6272ddcd159173ab09169078b86
sebastientromp <sebastien.tromp@gmail.com>
2019-02-05 12:01:08 +0100

    visual: move cards left in hand when hand size is big

--- src/js/components/game/hand.component.ts
+++ src/js/components/game/hand.component.ts
@@ -8,16 +8,18 @@ import { Entity } from '../../models/game/entity';
 	],
 	template: `
 		<ul class="hand">
-			<li *ngFor="let entity of _entities; trackBy: trackByFn">
+			<li *ngFor="let entity of _entities; let i = index; trackBy: trackByFn" 
+					[style.marginLeft.%]="i !== 0 ? marginLeft : 0">
 				<card [entity]="entity"></card>
 			</li>
 		</ul>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HandComponent implements AfterViewInit {
+export class HandComponent {
 
 	_entities: ReadonlyArray<Entity>;
+	marginLeft: number;
 
 	constructor(private elRef: ElementRef, private cdr: ChangeDetectorRef) {
 		// this.cdr.detach();
@@ -26,17 +28,25 @@ export class HandComponent implements AfterViewInit {
     @Input('entities') set entities(entities: ReadonlyArray<Entity>) {
         console.log('[hand] setting new entities', entities);
 		this._entities = entities;
+		switch (entities.length) {
+			case 7:
+				this.marginLeft = -3;
+				return;
+			case 8:
+				this.marginLeft = -5;
+				return;
+			case 9:
+				this.marginLeft = -7;
+				return;
+			case 10:
+				this.marginLeft = -9;
+				return;
+			default:
+				this.marginLeft = -1;
+		}
 	}
 	
 	trackByFn(index, item: Entity) {
 		return item.id;
-	}
-
-    ngAfterViewInit() {
-		// To trigger the resizing of all text elements
-		// setTimeout(() => {
-		// 	console.log('resizing');
-		// 	window.dispatchEvent(new Event('resize'));
-		// });
 	}
 }
