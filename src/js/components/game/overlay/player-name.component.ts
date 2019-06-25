6d4e34daa3c7a73620e8c0b4a01acbc1cd400f7f
sebastientromp <sebastien.tromp@gmail.com>
2019-06-25 10:31:16 +0200

    only resize player name on window:resize event

--- src/js/components/game/overlay/player-name.component.ts
+++ src/js/components/game/overlay/player-name.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input, ElementRef, ChangeDetectorRef, ViewRef } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, ElementRef, ChangeDetectorRef, ViewRef, HostListener } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { playerName } from '../../../../assets/svg/player_name';
@@ -28,10 +28,6 @@ export class PlayerNameComponent {
             private cdr: ChangeDetectorRef,
             private elRef: ElementRef) {
         this.svg = this.domSanitizer.bypassSecurityTrustHtml(playerName);
-        document.addEventListener(
-            'card-resize',
-            (event) => this.resizeText(),
-            true);
     }
 
     @Input('name') set name(value: string) {
@@ -44,16 +40,19 @@ export class PlayerNameComponent {
         this._active = value;
     }
 
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        this.resizeText();
+    }
+
     private resizeText() {
         const el = this.elRef.nativeElement.querySelector(".player-name");
         if (!el) {
             setTimeout(() => this.resizeText());
             return; 
         }
-        console.log('found element to resize player name', el);
         const fontSize = 0.17 * el.getBoundingClientRect().height;
         const textEl = this.elRef.nativeElement.querySelector(".text");
-        console.log('fontsize', fontSize, el.getBoundingClientRect());
         textEl.style.fontSize = fontSize + 'px';
         if (!(<ViewRef>this.cdr).destroyed) {
             this.cdr.detectChanges();
