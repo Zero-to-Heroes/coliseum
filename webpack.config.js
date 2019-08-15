72c63ebba23d4ddf3d872f87501cff6e30bfd8c7
sebastientromp <sebastien.tromp@gmail.com>
2019-08-15 08:36:49 +0200

    disable @typescript-eslint/no-var-requires for webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
-const webpack = require("@artonge/webpack");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require('@artonge/webpack');
 // const ngcWebpack = require("ngc-webpack");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const DefinePlugin = require("webpack").DefinePlugin;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const DefinePlugin = require('webpack').DefinePlugin;
 const AngularCompilerPlugin = webpack.AngularCompilerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-var path = require("path");
+var path = require('path');
 
-var _root = path.resolve(__dirname, ".");
+var _root = path.resolve(__dirname, '.');
 
 function getRoot(args) {
 	args = Array.prototype.slice.call(arguments, 0);
@@ -15,89 +17,87 @@ function getRoot(args) {
 }
 
 module.exports = function(env, argv) {
-	
 	return {
 		mode: env.production ? 'production' : 'development',
-		
+
 		entry: {
-			coliseum: "./src/js/modules/app/main.ts",
-			loader: "./src/js/modules/loader/main.ts",
+			coliseum: './src/js/modules/app/main.ts',
+			loader: './src/js/modules/loader/main.ts',
 		},
-		
-		target: "web",
-		
-		devtool: env.production ? false : "inline-source-map",
-		
+
+		target: 'web',
+
+		devtool: env.production ? false : 'inline-source-map',
+
 		watch: true,
-		
+
 		watchOptions: {
-			ignored: ['node_modules']
+			ignored: ['node_modules'],
 		},
-		
+
 		output: {
-			path: getRoot("dist"),
-			publicPath: "/",
-			filename: "[name].js"
+			path: getRoot('dist'),
+			publicPath: '/',
+			filename: '[name].js',
 		},
-		
+
 		resolve: {
 			// Having ts before js is important for webpack watch to work
 			// However, angular2-indexeddb creates an issue (ts files are packaged alongside js), so
 			// you need to remove the .ts files from its node_modules folder
 			// See https://github.com/gilf/angular2-indexeddb/issues/67
-			extensions: [".ts", ".js", ".html"]
+			extensions: ['.ts', '.js', '.html'],
 		},
-		
+
 		module: {
 			rules: [
 				{
 					test: /\.ts$/,
 					exclude: /node_modules/,
-					use: ['@artonge/webpack', 'eslint-loader']
+					use: ['@artonge/webpack', 'eslint-loader'],
 				},
 				{
 					test: /.js$/,
 					parser: {
-						system: true
-					}
+						system: true,
+					},
 				},
 				{
 					test: /\.scss$/,
-					include: getRoot("src", "css"),
-					use: ["raw-loader", "sass-loader"]
+					include: getRoot('src', 'css'),
+					use: ['raw-loader', 'sass-loader'],
 				},
 				{
 					test: /\.scss$/,
-					exclude: getRoot("src", "css"),
-					use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-				}
-			]
+					exclude: getRoot('src', 'css'),
+					use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				},
+			],
 		},
-		
+
 		plugins: [
 			new DefinePlugin({
 				'process.env.APP_VERSION': JSON.stringify(env.appversion),
-            }),
-			
+			}),
+
 			new AngularCompilerPlugin({
-				tsConfigPath: "./tsconfig.json",
-				entryModules: [
-					"./src/js/modules/app/app.module#AppModule",
-					"./src/js/modules/loader/loader.module#LoaderModule",
-				],
-				sourceMap: true
+				tsConfigPath: './tsconfig.json',
+				entryModules: ['./src/js/modules/app/app.module#AppModule', './src/js/modules/loader/loader.module#LoaderModule'],
+				sourceMap: true,
 			}),
-			
+
 			new MiniCssExtractPlugin({
-				filename: "coliseum.css"
+				filename: 'coliseum.css',
 			}),
-			
+
 			new CopyWebpackPlugin([
-				{ from: path.join(process.cwd(), "src/html/app.html") },
-				{ from: path.join(process.cwd(), "src/html/index.html") },
-				{ from: path.join(process.cwd(), "/../*") },
-				{ from: path.join(process.cwd(), "replay.xml") },
+				{ from: path.join(process.cwd(), 'src/html/app.html') },
+				{ from: path.join(process.cwd(), 'src/html/index.html') },
+				{ from: path.join(process.cwd(), '/../*') },
+				{ from: path.join(process.cwd(), 'replay.xml') },
 			]),
-		]
+
+			new BundleAnalyzerPlugin(),
+		],
 	};
 };
