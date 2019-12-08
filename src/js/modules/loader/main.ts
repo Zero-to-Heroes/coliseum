90caf03f025838fa7452050e5f94274e9c1ad541
sebastientromp <sebastien.tromp@gmail.com>
2019-12-08 21:36:23 +0100

    BUG: fix Coliseum not working in Edge

--- src/js/modules/loader/main.ts
+++ src/js/modules/loader/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import 'zone.js';
 import { LoaderModule } from './loader.module';
 
 platformBrowserDynamic().bootstrapModule(LoaderModule);
