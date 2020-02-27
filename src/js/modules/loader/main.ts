d2450134a753c6280150b9e74e17218520dab9ec
sebastientromp <sebastien.tromp@gmail.com>
2020-02-27 10:28:49 +0100

    DEV: enable prod mode in loader/main.ts

--- src/js/modules/loader/main.ts
+++ src/js/modules/loader/main.ts
@@ -1,5 +1,10 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import 'zone.js';
 import { LoaderModule } from './loader.module';
+
+if (process.env.NODE_ENV === 'production') {
+	enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(LoaderModule);
