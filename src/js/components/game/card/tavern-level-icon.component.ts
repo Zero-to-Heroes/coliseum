3ac0f5e0177cff3d9fafee0cd2fde2602d3e5de2
sebastientromp <sebastien.tromp@gmail.com>
2023-08-16 14:16:35 +0200

    use image instead of programmatically building the tavern tiers

--- src/js/components/game/card/tavern-level-icon.component.ts
+++ src/js/components/game/card/tavern-level-icon.component.ts
@@ -1,30 +1,29 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 
 @Component({
 	selector: 'tavern-level-icon',
 	styleUrls: ['../../../../css/components/game/card/tavern-level-icon.component.scss'],
 	template: `
 		<div class="tavern-level-icon">
-			<img
-				class="banner"
-				src="https://static.zerotoheroes.com/hearthstone/asset/coliseum/images/battlegrounds/tavern_banner.png"
-			/>
-			<div class="level">
-				<img
-					*ngFor="let number of stars"
-					src="https://static.zerotoheroes.com/hearthstone/asset/coliseum/images/battlegrounds/tavern_star.png"
-				/>
-			</div>
+			<img class="banner" [src]="image" />
 		</div>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TavernLevelIconComponent {
-	stars: readonly number[];
+	image: string;
 
 	@Input() set level(value: number) {
-		this.stars = Array(value)
-			.fill(0)
-			.map((x, i) => i);
+		this.image = value
+			? `https://static.zerotoheroes.com/hearthstone/asset/coliseum/images/battlegrounds/tavern_banner_${value}.png`
+			: 'https://static.zerotoheroes.com/hearthstone/asset/coliseum/images/battlegrounds/tavern_banner_0.png';
+
+		// if (!(this.cdr as ViewRef)?.destroyed) {
+		// 	this.cdr.detectChanges();
+		// }
+	}
+
+	constructor(private readonly cdr: ChangeDetectorRef) {
+		// cdr.detach();
 	}
 }
